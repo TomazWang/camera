6877ad19f74fb48c1458047e32e4d885080a6838
Tomaz Wang <tomazwang.tw@gmail.com>
2015-01-21 12:06:07 +0000

    add mobile ver

---
+++ includes/mobile.js
@@ -0,0 +1,17 @@
+function redirect(murl,weburl) {
+
+    try {
+        if (document.getElementById("bdmark") != null) {
+            return;
+        }
+        var urlhash = window.location.hash;
+        if (!urlhash.match("fromapp")) {
+            if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i))) {
+                location.replace(murl);
+            }else{
+                location.replace(weburl);
+            }
+        }
+    }
+    catch (err) {}
+}
---
+++ m/camera_m_edgeActions.js
@@ -0,0 +1,87 @@
+/***********************
+ * Adobe Edge Animate Composition Actions
+ *
+ * Edit this file with caution, being careful to preserve
+ * function signatures and comments starting with 'Edge' to maintain the
+ * ability to interact with these actions from within Adobe Edge Animate
+ *
+ ***********************/
+(function($, Edge, compId) {
+   var Composition = Edge.Composition,
+      Symbol = Edge.Symbol; // aliases for commonly used Edge classes
+
+   //Edge symbol: 'stage'
+   (function(symbolName) {
+
+      var isCameraDown = false;
+
+      Symbol.bindElementAction(compId, symbolName, "${camera_body}", "mousedown", function(sym, e) {
+         console.log("mousedown on camera - from edge animate mobile");
+         $("#Stage_camera_body").css("box-shadow", "rgba(0, 0, 0, 0.458824) 0px 0px 0px 0px");
+         $("#Stage_camera_body").css("top", "16px").css("left", "23px");
+         isCameraDown = true;
+      });
+
+
+      Symbol.bindElementAction(compId, symbolName, "${camera_body}", "mouseup", function(sym, e) {
+         console.log("mouseup on camera - from edge animate mobile");
+         $("#Stage_camera_body").css("box-shadow", "rgba(0, 0, 0, 0.458824) 23px 16px 16px 0px");
+         $("#Stage_camera_body").css("top", "0px").css("left", "0px");
+         isCameraDown = false;
+      });
+
+
+      Symbol.bindElementAction(compId, symbolName, "${camera_body}", "mouseout", function(sym, e) {
+
+         if (isCameraDown) {
+            console.log("mouseout on camera - from edge animate mobile");
+            $("#Stage_camera_body").css("box-shadow", "rgba(0, 0, 0, 0.458824) 23px 16px 16px 0px");
+            $("#Stage_camera_body").css("top", "0px").css("left", "0px");
+            isCameraDown = false;
+         }
+
+      });
+
+      Symbol.bindElementAction(compId, symbolName, "${camera_body}", "click", function(sym, e) {
+         
+         $.getScript("../includes/main_action.js",function(){
+            main_action(sym,true);
+         });
+         console.log("click on camera - from edge animate mobile");
+
+      });
+
+      Symbol.bindTriggerAction(compId, symbolName, "Default Timeline", 2000, function(sym, e) {
+         sym.stop();
+      });
+
+      //Edge binding end
+
+   })("stage");
+   //Edge symbol end:'stage'
+
+   //=========================================================
+
+   //Edge symbol: 'main_circle'
+   (function(symbolName) {
+
+   })("main_circle");
+   //Edge symbol end:'main_circle'
+
+   //=========================================================
+
+   //Edge symbol: 'circles'
+   (function(symbolName) {
+
+   })("circles");
+   //Edge symbol end:'circles'
+
+   //=========================================================
+
+   //Edge symbol: 'lence'
+   (function(symbolName) {
+
+   })("lence");
+   //Edge symbol end:'lence'
+
+})(window.jQuery || AdobeEdge.$, AdobeEdge, "EDGE-17128933");
